f2d2fe9ba777b3ee156ce0da183963c95ed8b6cf
jirhiker <jirhiker@gmail.com>
2020-08-26 15:19:13 -0400

    repo updated for analysis 1-16

--- scripts/sft/sft_DEBUG.py
+++ scripts/sft/sft_DEBUG.py
@@ -1,8 +1,17 @@
 def main():
     
+    #Open SEM
     raw_spectrometer_command('SetParameter ESA pos. Set, 528.6')
     raw_spectrometer_command('SetParameter ESA neg. Set, 528.6')
     raw_spectrometer_command('SetParameter CDD Supply Set, 1950.6')
+    
+    
+    sleep(20)
+    
+    #Protect SEM
+    raw_spectrometer_command('SetParameter ESA pos. Set, 0')
+    raw_spectrometer_command('SetParameter ESA neg. Set, 0')
+    raw_spectrometer_command('SetParameter CDD Supply Set, 0')
     
 # ========== EOF ==========
     
