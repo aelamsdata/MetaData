850167108ca5b652c12ee04f21e521715fe195fb
jirhiker <jirhiker@gmail.com>
2020-08-26 15:16:55 -0400

    repo updated for analysis 1-15

--- scripts/sft/sft_DEBUG.py
+++ scripts/sft/sft_DEBUG.py
@@ -1,9 +1,8 @@
 def main():
     
-    #raw_spectrometer_command('SetParameter ESA pos. Set, 528.6')
-    #raw_spectrometer_command('SetParameter ESA neg. Set, 528.6')
-    
-    raw_spectrometer_command('ActivateIonCounter SEM')
+    raw_spectrometer_command('SetParameter ESA pos. Set, 528.6')
+    raw_spectrometer_command('SetParameter ESA neg. Set, 528.6')
+    raw_spectrometer_command('SetParameter CDD Supply Set, 1950.6')
     
 # ========== EOF ==========
     
