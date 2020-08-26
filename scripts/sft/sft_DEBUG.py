cb40319caf57da43641b3885d3f0e9c876408a49
jirhiker <jirhiker@gmail.com>
2020-08-26 13:36:43 -0400

    repo updated for analysis 1-09

---
+++ scripts/sft/sft_DEBUG.py
@@ -0,0 +1,10 @@
+def main():
+    
+    
+    raw_spectrometer_command('SetParameter ESA pos. Set 528.6')
+    raw_spectrometer_command('SetParameter ESA neg. Set 528.6')
+    
+    
+# ========== EOF ==========
+    
+    
