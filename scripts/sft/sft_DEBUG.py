aba401da92f8ff8994a4c7777c1967874374b764
jirhiker <jirhiker@gmail.com>
2020-08-26 14:38:33 -0400

    repo updated for analysis 1-13

--- scripts/sft/sft_DEBUG.py
+++ scripts/sft/sft_DEBUG.py
@@ -1,9 +1,9 @@
 def main():
     
+    #raw_spectrometer_command('SetParameter ESA pos. Set, 528.6')
+    #raw_spectrometer_command('SetParameter ESA neg. Set, 528.6')
     
-    raw_spectrometer_command('SetParameter ESA pos. Set, 528.6')
-    raw_spectrometer_command('SetParameter ESA neg. Set, 528.6')
-    
+    raw_spectrometer_command('ActivateIonCounter')
     
 # ========== EOF ==========
     
